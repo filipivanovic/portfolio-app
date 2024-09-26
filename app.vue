0c07aa46c253e1fee38ef2645835f8fd260d0129
Filip Ivanovic <filip_ivanovic@hotmail.com>
2024-09-26 11:05:12 +0200

    Layout setup

--- app.vue
+++ app.vue
@@ -1,15 +1,11 @@
+<script setup>
+
+// const layout = 'another'
+
+</script>
+
 <template>
-  <nav>
-    <ul>
-      <li>
-        <NuxtLink to="/">Main</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/about">About</NuxtLink>
-      </li>
-    </ul>
-  </nav>
-  <div>
+  <NuxtLayout>
     <NuxtPage />
-  </div>
+  </NuxtLayout>
 </template>
---
+++ layouts/another.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <div>thiis is another layout</div>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
